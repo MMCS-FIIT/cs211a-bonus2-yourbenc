1985e2171d6f5284c97dd95c7f7c1349dc5f38a0
yourbenc <comeonaye@mail.ru>
2024-05-11 22:17:20 +0300

    First version

--- SimpleTGBot/TelegramBot.cs
+++ SimpleTGBot/TelegramBot.cs
@@ -1,6 +1,7 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using Telegram.Bot.Types.ReplyMarkups;
 
 namespace SimpleTGBot;
+
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -9,9 +10,13 @@ using Telegram.Bot.Types.Enums;
 
 public class TelegramBot
 {
-    // Токен TG-бота. Можно получить у @BotFather
-    private const string BotToken = "ВАШ_ТОКЕН_ИДЕНТИФИКАЦИИ_БОТА";
-    
+    private const string BotToken = "7058950530:AAEGodQkvldWalK2ukcQU2CxDvqUiPhWFVY";
+    public string separator = " ";
+    public string left_frame = "";
+    public string right_frame = "";
+    private States state = States.Normal;
+
+
     /// <summary>
     /// Инициализирует и обеспечивает работу бота до нажатия клавиши Esc
     /// </summary>
@@ -19,19 +24,18 @@ public class TelegramBot
     {
         // Если вам нужно хранить какие-то данные во время работы бота (массив информации, логи бота,
         // историю сообщений для каждого пользователя), то это всё надо инициализировать в этом методе.
-        // TODO: Инициализация необходимых полей
-        
+
         // Инициализируем наш клиент, передавая ему токен.
         var botClient = new TelegramBotClient(BotToken);
-        
+
         // Служебные вещи для организации правильной работы с потоками
         using CancellationTokenSource cts = new CancellationTokenSource();
-        
+
         // Разрешённые события, которые будет получать и обрабатывать наш бот.
         // Будем получать только сообщения. При желании можно поработать с другими событиями.
         ReceiverOptions receiverOptions = new ReceiverOptions()
         {
-            AllowedUpdates = new [] { UpdateType.Message }
+            AllowedUpdates = new[] { UpdateType.Message }
         };
 
         // Привязываем все обработчики и начинаем принимать сообщения для бота
@@ -45,14 +49,14 @@ public class TelegramBot
         // Проверяем что токен верный и получаем информацию о боте
         var me = await botClient.GetMeAsync(cancellationToken: cts.Token);
         Console.WriteLine($"Бот @{me.Username} запущен.\nДля остановки нажмите клавишу Esc...");
-        
+
         // Ждём, пока будет нажата клавиша Esc, тогда завершаем работу бота
-        while (Console.ReadKey().Key != ConsoleKey.Escape){}
+        while (Console.ReadKey().Key != ConsoleKey.Escape) { }
 
         // Отправляем запрос для остановки работы клиента.
         cts.Cancel();
     }
-    
+
     /// <summary>
     /// Обработчик события получения сообщения.
     /// </summary>
@@ -67,29 +71,168 @@ public class TelegramBot
         {
             return;
         }
-        // Будем обрабатывать только текстовые сообщения.
-        // При желании можно обрабатывать стикеры, фото, голосовые и т. д.
-        //
-        // Обратите внимание на использованную конструкцию. Она эквивалентна проверке на null, приведённой выше.
-        // Подробнее об этом синтаксисе: https://medium.com/@mattkenefick/snippets-in-c-more-ways-to-check-for-null-4eb735594c09
-        if (message.Text is not { } messageText)
+        // мой маркап
+        ReplyKeyboardMarkup framesMarkUp = new(new[]
+                {
+                    new KeyboardButton[] { "xXx_npuMep_xXx", "꧁npuMep꧂" },
+                    new KeyboardButton[] { "☜❶☞npuMep☜❶☞","npuMep" },
+                })
         {
-            return;
-        }
-
-        // Получаем ID чата, в которое пришло сообщение. Полезно, чтобы отличать пользователей друг от друга.
+            ResizeKeyboard = true
+        };
+        // Получаем ID чата, в которое пришло сообщение.
         var chatId = message.Chat.Id;
-        
-        // Печатаем на консоль факт получения сообщения
-        Console.WriteLine($"Получено сообщение в чате {chatId}: '{messageText}'");
 
-        // TODO: Обработка пришедших сообщений
-        
-        // Отправляем обратно то же сообщение, что и получили
-        Message sentMessage = await botClient.SendTextMessageAsync(
-            chatId: chatId,
-            text: "Ты написал:\n" + messageText,
-            cancellationToken: cancellationToken);
+        if (state == States.Normal)
+        {
+            if (message.Text != null)
+            {
+                if (message.Text.ToLower() == "/start")
+                {
+                    Message sentMessage = await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: $"На сервере запрещено:\n*использование читов, распрыжек, скриптов и прочих хитростей;\n*использование спреев - обманок и спреев с эротическим содержанием;\nиспользование уязвимостей карт;\n*мат в любом проявлении\n(слово сука хоть и литературное, но на сервере расценивается как мат);\n*оскорбления игроков;\n*использование нечитаемых ников, ников содежащих мат, цифровых ников.\nВсё это карается баном\n\nСписок команд:\n/ChangeSeparator - добавить разделитель, ставящийся между словами(крутым парням рекомендуется точка или тире) - по умолчанию пробел\n/ChangeFrames - выбрать рамки для сообщений",
+                        cancellationToken: cancellationToken);
+                }
+                else if (message.Text.ToLower() == "/changeseparator")
+                {
+                    Message sentMessage = await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: $"Введите новый разделитель(если хотите вернуть пробел, то напишите 'пробел')",
+                        cancellationToken: cancellationToken);
+                    state = States.SeparatorChange;
+                    return;
+                }
+                else if (message.Text.ToLower() == "/changeframes")
+                {
+                    Message sentMessage = await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Выберите рамку из предложенного списка:",
+                    replyMarkup: framesMarkUp,
+                    cancellationToken: cancellationToken);
+                    state = States.FrameChange;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Получено сообщение в чате {chatId}: '{message.Text}'");
+                    Message sentMessage = await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: Functions.TranslateMessage(message.Text, separator, left_frame, right_frame),
+                        cancellationToken: cancellationToken);
+                }
+            }
+            else if (message.Sticker != null)
+            {
+                Console.WriteLine($"Получено сообщение в чате {chatId}: стикер");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Классный стикер бро",
+                cancellationToken: cancellationToken);
+            }
+            else if (message.Location != null)
+            {
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Адрес записан, ожидайте гостей",
+                cancellationToken: cancellationToken);
+            }
+            else if (message.Video != null)
+            {
+                Console.WriteLine($"Получено сообщение в чате {chatId}: видео");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Классное видео братан я под столом",
+                cancellationToken: cancellationToken);
+            }
+        }
+        // Состояние смены разделителя
+        if (state == States.SeparatorChange)
+        {
+            if (message.Text == null)
+            {
+                Console.WriteLine($"Попытка изменить разделитель в чате {chatId}: неудача");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Что-то так себе разделитель. Давай по-новой",
+                cancellationToken: cancellationToken);
+            }
+            else
+            {
+                if (message.Text.ToLower() == "пробел")
+                    separator = " ";
+                else
+                    separator = message.Text;
+                Console.WriteLine($"Попытка изменить разделитель в чате {chatId}: успешно");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Разделитель обновлён",
+                cancellationToken: cancellationToken);
+                state = States.Normal;
+            }
+        }
+        // Состояние смены рамки
+        if (state == States.FrameChange)
+        {
+
+            if (message.Text == "xXx_npuMep_xXx")
+            {
+                Console.WriteLine($"Попытка изменить рамку в чате {chatId}: успех");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Рамка обновлена",
+                replyMarkup: new ReplyKeyboardRemove(),
+                cancellationToken: cancellationToken);
+                left_frame = "xXx_";
+                left_frame = "_xXx";
+                state = States.Normal;
+            }
+            else if (message.Text == "꧁npuMep꧂")
+            {
+                Console.WriteLine($"Попытка изменить рамку в чате {chatId}: успех");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Рамка обновлена",
+                replyMarkup: new ReplyKeyboardRemove(),
+                cancellationToken: cancellationToken);
+                left_frame = "꧁";
+                right_frame = "꧂";
+                state = States.Normal;
+            }
+            else if (message.Text == "☜❶☞npuMep☜❶☞")
+            {
+                Console.WriteLine($"Попытка изменить рамку в чате {chatId}: успех");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Рамка обновлена",
+                replyMarkup: new ReplyKeyboardRemove(),
+                cancellationToken: cancellationToken);
+                left_frame = "☜❶☞";
+                right_frame = "☜❶☞";
+                state = States.Normal;
+            }
+            else if (message.Text == "npuMep")
+            {
+                Console.WriteLine($"Попытка изменить рамку в чате {chatId}: успех");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Рамка обновлена",
+                replyMarkup: new ReplyKeyboardRemove(),
+                cancellationToken: cancellationToken);
+                left_frame = "";
+                right_frame = "";
+                state = States.Normal;
+            }
+            else
+            {
+                Console.WriteLine($"Попытка изменить рамку в чате {chatId}: неудача");
+                Message newMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "НУ КАКАЯ ЭТО РАМКА????? Выберите, пожалуйста, нормальную",
+                replyMarkup: framesMarkUp,
+                cancellationToken: cancellationToken);
+            }
+        }
     }
 
     /// <summary>
@@ -106,12 +249,12 @@ public class TelegramBot
         {
             ApiRequestException apiRequestException
                 => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-            
+
             _ => exception.ToString()
         };
 
         Console.WriteLine(errorMessage);
-        
+
         // Завершаем работу
         return Task.CompletedTask;
     }
